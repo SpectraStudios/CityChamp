eeff4bda590c6de93eb9c1618452e7b0653569d9
Matt Kudo <mcrittenden33@gmail.com>
2024-03-25 11:30:18 +0900

    Added more levels

---
+++ Assets/_CityChamp/Scripts/Core/Player/Combat/Move.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpectraStudios.CityChamp
+{
+    public class Move : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
--- Assets/_CityChamp/Scripts/Core/StatesAndManagers/ScenesManager.cs
+++ Assets/_CityChamp/Scripts/Core/StatesAndManagers/ScenesManager.cs
@@ -1,13 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace SpectraStudios.CityChamp
 {
     public class ScenesManager : MonoBehaviour
     {
-        /*[SerializeField] private SceneField 
+        //[SerializeField] private SceneField;
+
+        [SerializeField] private GameObject _levelPanel;
+        [SerializeField] private GameObject _loadingPanel;
+        [SerializeField] private Image _progressBar;
+
+        private string _currentLevelScene;
+
+        private static ScenesManager _instance;
+        public static ScenesManager Instance
+        {
+            get
+            {
+                /*if (_instance == null)
+                { Debug.LogError("Scenes Manager is NULL.");
+                }*/
+                return _instance;
+            }
+        }
 
         private void Awake()
+        {
+            if (Instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        /*private void Awake()
         {
             GameManager.OnGameStateChanged += ;
         }
@@ -19,7 +51,94 @@ namespace SpectraStudios.CityChamp
 
         private void Start()
         {
-            SceneManager.LoadScene("Level2", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
+            _currentLevelScene = "Level1";
+        }
+
+
+        private void ChangeScene(string sceneName)
+        {
+
+        }
+
+        private void UnloadScene(string sceneName)
+        {
+            SceneManager.UnloadSceneAsync(_currentLevelScene);
+        }
+
+        private async void LoadScene(string sceneName)
+        {
+            var scene = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            scene.allowSceneActivation = false;
+
+            _levelPanel.SetActive(false);
+            _loadingPanel.SetActive(true);
+
+            do
+            {
+                _progressBar.fillAmount = scene.progress;
+            } while (scene.progress < 0.9f);
+
+            scene.allowSceneActivation = true;
+            _loadingPanel.SetActive(false);
+
+            _currentLevelScene = sceneName;
+
+            _levelPanel.SetActive(true);
+        }
+
+        public void GoToLevel1()
+        {
+            if (_currentLevelScene != "Level1")
+            {
+                UnloadScene(_currentLevelScene);
+                LoadScene("Level1");
+            }
+        }
+
+        public void GoToLevel2()
+        {
+            if (_currentLevelScene != "Level2")
+            {
+                UnloadScene(_currentLevelScene);
+                LoadScene("Level2");
+            }
+        }
+
+        public void GoToLevel3()
+        {
+            if (_currentLevelScene != "Level3")
+            {
+                UnloadScene(_currentLevelScene);
+                LoadScene("Level3");
+            }
+        }
+
+        public void GoToLevel4()
+        {
+            if (_currentLevelScene != "Level4")
+            {
+                UnloadScene(_currentLevelScene);
+                LoadScene("Level4");
+            }
+        }
+
+        public void GoToLevel5()
+        {
+            if (_currentLevelScene != "Level5")
+            {
+                UnloadScene(_currentLevelScene);
+                LoadScene("Level5");
+            }
+        }
+
+        public void GoToLevel6()
+        {
+            if (_currentLevelScene != "Level6")
+            {
+                UnloadScene(_currentLevelScene);
+                LoadScene("Level6");
+            }
         }
     }
 }
